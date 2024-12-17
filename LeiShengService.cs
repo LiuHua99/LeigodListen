8f4d57b8f329678453e960f691067586701bc11c
282411824 <282411824@qq.com>
2024-12-17 16:23:45 +0800

    添加项目文件。

---
+++ LeiShengService.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Linq;
+using LeiShen;
+
+public class LeiShengService
+{
+    //错误码 -1 token失效
+    //错误码 -2 未知错误
+    //其他错误码参考如下
+    public const int HTTP_SUCCESS_NET_CODE = 0;
+    public const int HTTP_TOKEN_EXPIRE = 400006;
+    public const int HTTP_LOGIN_ERROR_CODE = 400003;
+    public const int HTTP_ERROR_NEW_CODE = 0x5e4;
+    public const int HTTP_ERROR_NOT_PAY = 400877;
+    public const int HTTP_ERROR_WX_NOBIND = 400617;
+    public const int HTTP_ERROR_JYCODE = 400857;
+    public const int EXPERIENCE_END_TIME = 400855;
+
+
+
+    private const string loginURL = "https://webapi.leigod.com/api/auth/login/v1";
+    private const string pauseURL = "https://webapi.leigod.com/api/user/pause";
+    private const string InfoURL = "https://webapi.leigod.com/api/user/info";
+    private const string KEY = "5C5A639C20665313622F51E93E3F2783";
+
+    private readonly HttpClient httpClient;
+    private readonly Dictionary<string, string> header;
+    private string accountToken = "";
+    private DateTime experienceEndTime = DateTime.MinValue;
+    public LeiShengService(string accountToken = null)
+    {
+        this.accountToken = accountToken;
+        httpClient = new HttpClient();
+        header = new Dictionary<string, string>
+        {
+           {"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.53"},
+           {"Content-Type",  "application/x-www-form-urlencoded; charset=UTF-8"},
+           {"Connection",  "keep-alive"},
+           {"Accept",  "application/json, text/javascript, */*; q=0.01"},
+           {"Accept-Encoding",  "gzip, deflate, br"},
+           {"Accept-Language",  "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6"},
+           {"DNT",  "1"},
+           {"Referer",  "https://www.legod.com/"},
+           {"Sec-Fetch-Dest",  "empty"},
+           {"Sec-Fetch-Mode",  "cors"},
+           {"Sec-Fetch-Site",  "same-site" },
+        };
+    }
+
+    public DateTime GetExperienceEndTime()
+    {
+        return experienceEndTime;
+    }
+    public string GetAccountToken()
+    {
+        return accountToken;
+    }
+
+    public async Task<KeyValuePair<bool, string>> LoginAsync(string uname, string password)
+    {
+        // 输入校验
+        if (string.IsNullOrEmpty(uname) || string.IsNullOrEmpty(password))
+        {
+            return new KeyValuePair<bool, string>(false, "用户名或密码不能为空");
+        }
+
+        // 构造请求体
+        var body = new Dictionary<string, string>
+        {
+            { "account_token", "null" },
+            { "country_code", "86" },
+            { "lang", "zh_CN" },
+            { "mobile_num", uname },
+            { "os_type", "4" },
+            { "password", GenerateMD5(password) },
+            { "region_code", "1" },
+            { "src_channel", "guanwang" },
+            { "username", uname }
+        };
+
+        // 对请求体做签名
+        LegodSign(body);
+
+        // 将请求体转换为 URL 编码格式
+        var content = new FormUrlEncodedContent(body);
+
+        try
+        {
+            // 发送 POST 请求
+            var response = await httpClient.PostAsync(loginURL, content);
+            response.EnsureSuccessStatusCode(); // 确保返回状态为 2xx
+
+            // 解析响应内容
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var msg = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonResponse);
+
+            if (msg.TryGetValue("code", out var code) && Convert.ToInt32(code) == 0)
+            {
+                dynamic data = msg["data"];
+                var token = data.login_info.account_token.ToString();
+                accountToken = token;
+                return new KeyValuePair<bool, string>(true, token);
+            }
+            else
+            {
+                return new KeyValuePair<bool, string>(false, "errorCode: {code}  " + (msg.TryGetValue("msg", out object value) ? value.ToString() : "未知错误"));
+            }
+        }
+        catch (HttpRequestException e)
+        {
+            return new KeyValuePair<bool, string>(false, $"请求失败: {e.Message}");
+        }
+        catch (Exception e)
+        {
+            return new KeyValuePair<bool, string>(false, $"发生错误: {e.Message}");
+        }
+    }
+
+
+    public async Task<int> PauseAsync()
+    {
+        // 请求暂停
+        var payload = new Dictionary<string, string>
+            {
+                { "account_token",  this.accountToken },
+                { "lang", "zh_CN" },
+                { "os_type", "4" }
+            };
+
+        try
+        {
+            var content = new FormUrlEncodedContent(payload);
+            var response = await httpClient.PostAsync(pauseURL, content);
+            if (response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+            {
+                return -1;
+            }
+
+            // 解析响应内容
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            dynamic msg = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonResponse);
+            Program.ConsoleWriteLine("暂停结果：" + msg["msg"].ToString());
+            if (!msg.TryGetValue("code", out object code))
+            {
+                return -2;
+            }
+            var intcode = Convert.ToInt32(code);
+            return intcode;
+        }
+        catch (HttpRequestException ex)
+        {
+            // 未知错误
+            return 404;
+        }
+        catch (Exception ex)
+        {
+            Program.ConsoleWriteLine("未知错误，可能是请求频繁或者是网址更新: " + ex.Message);
+            return -2;
+        }
+    }
+
+    public async Task<int> GetIsPause()
+    {
+        // 构建请求体
+        var payload = new Dictionary<string, string>
+        {
+            { "account_token", this.accountToken },
+            { "lang", "zh_CN" },
+            { "os_type", "4" }
+        };
+
+        try
+        {
+            // 发送 POST 请求
+            var content = new FormUrlEncodedContent(payload);
+            var response = await httpClient.PostAsync(InfoURL, content);
+            response.EnsureSuccessStatusCode();  // 确保 HTTP 请求成功
+
+            // 解析响应内容
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var msg = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonResponse);
+            msg.TryGetValue("code", out var code);
+            if (Convert.ToInt32(code) == HTTP_TOKEN_EXPIRE)
+            {
+                return -1; //登陆失败
+            }
+            else if (Convert.ToInt32(code) == 0)
+            {
+                dynamic msgdata = msg["data"];
+                try
+                {
+                    var experienceTime = msgdata.expired_experience_time.Value.ToString();
+                    this.experienceEndTime = DateTime.Parse(experienceTime);
+                    if (experienceEndTime > DateTime.Now)
+                    {
+                        return EXPERIENCE_END_TIME;
+                    }
+                }
+                catch { }
+
+                var ispause = msgdata.pause_status_id.Value;
+                // 返回状态
+                return int.Parse(ispause.ToString());
+            }
+            else
+            {
+                // 未知错误
+                return Convert.ToInt32(code);
+            }
+        }
+        catch (HttpRequestException ex)
+        {
+            // 未知错误
+            return 404;
+        }
+        catch (Exception ex)
+        {
+            // 未知错误
+            return -2;
+        }
+    }
+
+
+    // MD5 加密
+    private string GenerateMD5(string input)
+    {
+        using (var md5 = MD5.Create())
+        {
+            var inputBytes = Encoding.UTF8.GetBytes(input);
+            var hashBytes = md5.ComputeHash(inputBytes);
+            var sb = new StringBuilder();
+            foreach (var b in hashBytes)
+            {
+                sb.Append(b.ToString("x2")); // 转换为16进制字符串
+            }
+            return sb.ToString();
+        }
+    }
+
+    // 签名请求体的示例方法
+    private void LegodSign(Dictionary<string, string> body)
+    {
+
+
+        // body["ts"] = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+        body["ts"] = ((long)(DateTime.Now - new DateTime(1970, 1, 1, 8, 0, 0)).TotalSeconds).ToString();
+
+
+        // 添加时间戳
+
+
+        // 生成待签名字符串
+        var sortedParams = body
+            .OrderBy(kvp => kvp.Key) // 按键顺序排序
+            .Select(kvp => $"{kvp.Key}={kvp.Value}") // 将键值对转换为字符串格式
+            .ToArray();
+
+        // 将所有参数连接为一个字符串，并添加密钥
+        string strToSign = string.Join("&", sortedParams) + "&key=" + KEY;
+
+        // 打印待签名字符串（调试时使用）
+        Program.ConsoleWriteLine("sign: " + strToSign);
+
+        // 生成签名并添加到请求体
+        body["sign"] = GenerateMD5(strToSign);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,370 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeiShen
+{
+    internal class Program
+    {
+        static string ConfigFile = "配置.ini";
+        static LeiShengService authService;
+        private static IntPtr selfHandle;
+        static NotifyIcon notifyIcon;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            ConfigFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigFile);
+            NotifyIconInit();
+            // 设置关闭事件的回调处理函数
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+            var isAutoRun = ConfigHelper.INIRead("配置", "开机启动", ConfigFile);
+            SetAutoRun(isAutoRun == "1");
+            FirstRun();
+            CreateProcessStopListen();
+            Application.Run();
+        }
+
+  
+
+        private static void NotifyIconInit()
+        {
+            selfHandle = GetConsoleWindow();
+            ShowWindow(selfHandle, SW_HIDE);
+
+            //初始化 NotifyIcon
+            notifyIcon = new NotifyIcon
+            {
+                Icon = SystemIcons.Application, // 这里可以替换为你自己的图标
+                Visible = true,
+                Text = "雷神加速器监听器"
+            };
+
+            // 创建右键菜单
+            var contextMenu = new ContextMenu();
+            contextMenu.MenuItems.Add(new MenuItem("退出程序", (s, e) => Exit()));
+            notifyIcon.ContextMenu = contextMenu;
+            // 订阅双击事件
+            notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
+        }
+
+        private static void Exit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            ShowWindow(selfHandle, SW_SHOW);
+            notifyIcon.Visible = false;
+        }
+
+        private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            var isAutoRun = ConfigHelper.INIRead("配置", "开机启动", ConfigFile);
+            SetAutoRun(isAutoRun == "1");
+        }
+
+        private static void SetAutoRun(bool isAutoRun = true)
+        {
+            // 获取注册表键值
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+            string appName = "LeigodListen";
+            if (isAutoRun)
+            {
+                string appPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, AppDomain.CurrentDomain.FriendlyName);
+                // 添加开机自启动项
+                key.SetValue(appName, appPath);
+                ConsoleWriteLine("已设置开机自启动!");
+            }
+            else
+            {
+                var names = key.GetValueNames();
+                if (names.Contains(appName))
+                {
+                    key.DeleteValue(appName);
+                    ConsoleWriteLine("已关闭开机自启动!");
+                }
+            }
+        }
+
+        private static void CreateProcessStopListen()
+        {
+            ManagementEventWatcher StartedWatcher = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace"));
+            StartedWatcher.EventArrived += new EventArrivedEventHandler(HandleProcessStart);
+            StartedWatcher.Start();
+
+            ManagementEventWatcher StopWatcher = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStopTrace"));
+            StopWatcher.EventArrived += new EventArrivedEventHandler(HandleProcessStop);
+            StopWatcher.Start();
+        }
+
+        private static void HandleProcessStart(object sender, EventArrivedEventArgs e)
+        {
+            var pname = e.NewEvent["ProcessName"].ToString();
+            if (pname == "leigod.exe")
+            {
+                var nlgodintpr = GetProcesses("leigod.exe");
+                var nlgodpid = GetProcessId(nlgodintpr);
+
+                if (nlgodpid == LeigodPID || nlgodintpr == LeigodIntPtr)
+                    return;
+
+                LeigodIntPtr = nlgodintpr;
+                LeigodPID = nlgodpid;
+
+                if (LeigodPID == 0)
+                    return;
+
+                ConsoleWriteLine($"监听到加速器启动! pid: {LeigodPID} Name： {pname}");
+            }
+        }
+
+
+        public static void ConsoleWriteLine(string msg)
+        {
+            Console.WriteLine($"[{DateTime.Now}]: {msg}");
+        }
+
+
+        private static async void HandleProcessStop(object sender, EventArrivedEventArgs e)
+        {
+            if (LeigodIntPtr == IntPtr.Zero)
+                return;
+            var pname = e.NewEvent["ProcessName"].ToString();
+            if (pname != "leigod.exe")
+                return;
+
+            var pid = (uint)(e.NewEvent["ProcessID"]);
+            //WriteLine($"0 ProcessID:{pid} ProcessName:{pname}");
+            if (LeigodPID == pid)
+            {
+                ConsoleWriteLine($"监听到加速器退出! pid: {pid}  Name：{pname}");
+
+                while (!await StopLeigodTime())
+                {
+                    if (errorCount >= 3)
+                    {
+                        MessageBox.Show("出错次数超过3次!请检查网络或者配置!", "警告", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    errorCount++;
+                    ConsoleWriteLine($"失败!正在进行第[{errorCount}]次尝试.");
+                    Thread.Sleep(TimeSpan.FromSeconds(3));
+                }
+
+            }
+        }
+
+
+        private static async void FirstRun()
+        {
+            var user = ConfigHelper.INIRead("配置", "账号", ConfigFile);
+            var pwd = ConfigHelper.INIRead("配置", "密码", ConfigFile);
+            var token = ConfigHelper.INIRead("配置", "Token", ConfigFile);
+            authService = new LeiShengService(token);
+            var pausestatus = await authService.GetIsPause();
+            if (pausestatus != 0 && pausestatus != 1 && pausestatus != LeiShengService.EXPERIENCE_END_TIME)
+            {
+                var loginmsg = await authService.LoginAsync(user, pwd);
+                if (!loginmsg.Key)
+                {
+                    ConsoleWriteLine(loginmsg.Value);
+                    ConsoleWriteLine("登陆失败");
+                    Console.ReadKey();
+                    Process.GetCurrentProcess().Kill();
+                }
+                else
+                {
+                    ConsoleWriteLine("账号密码登陆成功");
+                    ConfigHelper.INIWrite("配置", "Token", loginmsg.Value, ConfigFile);
+                }
+            }
+            else
+            {
+                ConsoleWriteLine("Token登陆成功");
+            }
+            ConsoleWriteLine("开始监听........");
+            LeigodIntPtr = GetProcesses("leigod.exe");
+
+            if (LeigodIntPtr == IntPtr.Zero && pausestatus == 0)
+            {
+                ConsoleWriteLine("首次启动监听加速器时间没有暂停,将执行暂停!");
+                await StopLeigodTime();
+            }
+
+            if (LeigodIntPtr != IntPtr.Zero)
+            {
+                LeigodPID = GetProcessId(LeigodIntPtr);
+                ConsoleWriteLine("首次启动监听到加速器正在运行!");
+            }
+            else
+            {
+                ConsoleWriteLine("首次启动没有监听到加速器启动,将在后台持续监听!");
+            }
+        }
+
+        static int errorCount = 0;
+        private static async Task<bool> StopLeigodTime()
+        {
+            //为了支持运行过程中直接改配置生效
+            var user = ConfigHelper.INIRead("配置", "账号", ConfigFile);
+            var pwd = ConfigHelper.INIRead("配置", "密码", ConfigFile);
+
+            //如果雷神程序已经退出
+            var resultIsPause = await authService.GetIsPause();
+            switch (resultIsPause)
+            {
+                case -1://登录过期
+                    {
+                        ConsoleWriteLine("登录过期!尝试重新登录");
+                        var loginResult = await authService.LoginAsync(user, pwd);
+                        if (!loginResult.Key)
+                        {
+                            ConsoleWriteLine(loginResult.Value);
+                        }
+                        ConsoleWriteLine("重新登录成功");
+                    }
+                    break;
+                case 0://未暂停
+                    {
+                        ConsoleWriteLine("执行停止功能");
+                        var pauseResult = await authService.PauseAsync();
+                        return pauseResult != LeiShengService.HTTP_TOKEN_EXPIRE;
+                    }
+                    break;
+                case 1://已经暂停
+                    {
+
+                    }
+                    return true;
+                case 404:
+                    {
+                        //没网
+                        ConsoleWriteLine("无网络连接!");
+                    }
+                    break;
+                case LeiShengService.EXPERIENCE_END_TIME://无法暂停的体验时间
+                    {
+                        var experienceEndTime = authService.GetExperienceEndTime();
+                        var timespan = experienceEndTime - DateTime.Now;
+                        ConsoleWriteLine("当前为无法暂停的体验时间时间,直到:" + experienceEndTime);
+                        Thread.Sleep(timespan); //暂停检测直到体验时间结束
+
+                    }
+                    return true;
+                default: //累计未知错误次数
+                    break;
+            }
+
+            return false;
+        }
+
+        public static bool IsProcessRunning(IntPtr processHandle)
+        {
+            // 获取与句柄相关联的进程 ID
+            int processId = GetProcessId(processHandle);
+
+            try
+            {
+                Process process = Process.GetProcessById(processId);
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+            // 如果 PID 大于 0，则该进程正在运行
+            return true;
+        }
+
+        public static IntPtr LeigodIntPtr = IntPtr.Zero;
+        public static int LeigodPID = 0;
+
+        public static IntPtr GetProcesses(string exePath)
+        {
+            List<uint> listprocesses = new List<uint>();
+            // 遍历进程
+            uint[] processes = new uint[2048];
+            uint bytesNeeded;
+
+            // 获取所有进程 ID
+            EnumProcesses(processes, (uint)(processes.Length * sizeof(uint)), out bytesNeeded);
+            if (bytesNeeded > 2048)
+            {
+                processes = new uint[bytesNeeded];
+                EnumProcesses(processes, (uint)(processes.Length * sizeof(uint)), out bytesNeeded);
+            }
+            listprocesses.AddRange(processes);
+            listprocesses.RemoveRange((int)bytesNeeded, (int)(listprocesses.Count - bytesNeeded));
+
+            for (int i = 0; i < listprocesses.Count; i++)
+            {
+                IntPtr hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, (int)listprocesses[i]);
+                if (hProcess != IntPtr.Zero)
+                {
+                    System.Text.StringBuilder filePath = new System.Text.StringBuilder(1024);
+                    if (GetModuleFileNameEx(hProcess, IntPtr.Zero, filePath, filePath.Capacity))
+                    {
+                        string processPath = filePath.ToString();
+                        var tempname = processPath.Substring(processPath.Length - exePath.Length);
+                        // 比较路径（使用 StringComparison.OrdinalIgnoreCase 忽略大小写）
+                        if (tempname == exePath)
+                        {
+                            //ConsoleWriteLine($"找到进程: {Path.GetFileName(processPath)}  PID: {processes[i]}");
+                            return hProcess;
+                        }
+                    }
+
+                }
+            }
+
+            return IntPtr.Zero;
+        }
+
+
+        // 定义 Windows API 函数
+        // 定义委托，用于处理控制台关闭事件
+        private delegate bool ConsoleEventHandler(int eventType);
+        private static ConsoleEventHandler _handler; // 保持委托的引用，避免被垃圾回收
+
+        // 引入 Windows API
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool SetConsoleCtrlHandler(ConsoleEventHandler callback, bool add);
+
+        // 关闭事件类型
+        private const int CTRL_CLOSE_EVENT = 2;
+        // 导入 User32.dll
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+
+        [DllImport("kernel32.dll")]
+        private static extern int GetProcessId(IntPtr hProcess);
+
+        [DllImport("psapi.dll")]
+        private static extern bool EnumProcesses(uint[] lpidProcess, uint cb, out uint lpcbNeeded);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("psapi.dll")]
+        private static extern bool GetModuleFileNameEx(IntPtr hProcess, IntPtr hModule, System.Text.StringBuilder lpBaseName, int nSize);
+
+        private const uint PROCESS_QUERY_INFORMATION = 0x0400;
+        private const uint PROCESS_VM_READ = 0x0010;
+    }
+}
